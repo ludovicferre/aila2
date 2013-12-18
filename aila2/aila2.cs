0be027d0cf1af3ff8f64a6cdc6b6b6e2d41880b7
ludovic.ferre <ludovic.ferre@gmail.com>
2013-12-18 16:05:54 +0100

    Build up the schema parser.

--- aila2/aila2.cs
+++ aila2/aila2.cs
@@ -32,9 +32,49 @@ namespace Symantec.CWoC.aila2 {
 
     class SchemaParser {
         public string current_schema_string;
+        int[] field_positions;
 
         public SchemaParser() {
             current_schema_string = "";
+            field_positions = new int[10];
+        }
+
+        private readonly string[] SupportedFields = new string[] {
+            "date",
+            "time",
+            "cs-method",
+            "cs-uri-stem",
+            "cs-uri-query",
+            "cs-username",
+            "c-ip",
+            "sc-status",
+            "sc-substatus",
+            "sc-win32-status",
+            "time-taken"
+        };
+
+        public int ParseSchemaString (string schema) {
+            schema = schema.Substring(9);
+            if (schema != current_schema_string) {
+                current_schema_string = schema;
+                string[] fields = schema.Split(' ');
+                foreach (string f in fields) {
+                    int i = 0;
+                    foreach (string s in SupportedFields) {
+                        if (s == f) {
+                            field_positions.Add(i);
+                            //Console.WriteLine("{0}: {1}", i.ToString(), f);
+                            break;
+                        }
+                        i++;
+                    }
+                }
+                int j = 0;
+                foreach (int i in field_positions) {
+                    Console.WriteLine("{0}-{1}: {2}", j++.ToString(), i.ToString(), SupportedFields[i]);
+                }
+            }
+            return 0;
         }
 
     }
@@ -63,28 +103,26 @@ namespace Symantec.CWoC.aila2 {
 
 
     class LogAnalyzer {
+        private ResultSet results;
+        private SchemaParser schema;
+
         public void AnalyzeFile (string filename) {
             // Count lines
-            ResultSet results = new ResultSet();
-            SchemaParser schema = new SchemaParser();
+            results = new ResultSet();
+            schema = new SchemaParser();
             Timer.Init();
             try {
                 using (StreamReader r = new StreamReader(filename)){
+                    int i = 0;
                     while (r.Peek() >= 0) {
                         string line = r.ReadLine();
-                        if (line.StartsWith("#")) {
-                            if (line.StartsWith("#Fields:")) {
-                                if (schema.current_schema_string == "" || schema.current_schema_string != line) {
-                                    schema.current_schema_string = line;
-                                    results.SchemaDef++;
-                                }
-                                Console.WriteLine(line);
-                            }
-                        } else {
-                            results.DataLines++;
-                        }
-
+                        AnalyzeLine(line);
                         results.LineCount++;
+
+                        if (++i > 9999) {
+                            Console.Write("Processed {0} lines...\r", results.LineCount);
+                            i = 0;
+                        }
                     }
                 }
             } catch {
@@ -94,7 +132,17 @@ namespace Symantec.CWoC.aila2 {
         }
 
         private void AnalyzeLine(string line) {
-
+            if (line.StartsWith("#")) {
+                if (line.StartsWith("#Fields:")) {
+                    if (schema.current_schema_string == "" || schema.current_schema_string != line) {
+                        schema.ParseSchemaString(line.ToLower());
+                        results.SchemaDef++;
+                    }
+                    // Console.WriteLine(line);
+                }
+            } else {
+                results.DataLines++;
+            }
         }
     }
 }
