04e9ce7d28c205e154bbae93bbeac14f15b0af35
ludovic.ferre <ludovic.ferre@gmail.com>
2014-01-02 14:19:04 +0100

    Added proper command line handling (success error, help, version). Fixed the array output problem using a string builder and removing the last comma added.

--- aila2-web/aila2-siteconfig.cs
+++ aila2-web/aila2-siteconfig.cs
@@ -5,7 +5,7 @@ using System.Text;
 
 namespace  Symantec.CWoC {
     class aila2_web {
-        static void Main(string[] args) {
+        static int Main(string[] args) {
             // Parse the current directory or directory provided via -d and generate a json file listing all json files.
             // The file will be named siteconfig.json and excluded from the search ;).
 
@@ -13,6 +13,48 @@ namespace  Symantec.CWoC {
             int argc = argv.Length;
 
             string dir_path = string.Empty;
+
+            string help_message = @"
+Usage: aila2-siteconfig -i <json directory path>
+
+Output a json formated string to the console defining a sitename string, a
+schema_version int and a file_list string array (listing all json files found
+in the provided path), as shown in this sample:
+
+    {
+            ""sitename"" : ""aila2-web2"",
+            ""schema_version"" : 1,
+            ""file_list"" : [
+                    ""u_ex140101.json"",
+                    ""u_ex131231.json"",
+                    ""u_ex131230.json""
+            ]
+    }
+
+Additional command line options:
+
+    -v, --version   Display the tool version
+    /?, --help      Display the tool usage (this message).
+";
+            string version_message = "aila2-siteconfig version 1";
+
+            if (argc == 1) {
+                // Check if the caller is requesting version, if not print help
+                if (argv[0] == "-v" || argv[0] == "--version") {
+                    Console.WriteLine(version_message);
+                    return 0;
+                } else {
+                    Console.WriteLine(help_message);
+                    if (argv[0] == "/?" || argv[0] == "--help") {
+                        return 0; // Help requested - return success.
+                    } else {
+                        return -1; // Incorrect agrs provided. Return error.
+                    }
+                }
+            } else if (argc > 2) {
+                Console.WriteLine(help_message);
+                return -1;
+            }
 
             for (int i = 0; i < argc; i++) {
                 if (argv[i] == "-i" && argc > 1)
@@ -23,27 +65,25 @@ namespace  Symantec.CWoC {
                 dir_path = Directory.GetCurrentDirectory();
 
             string filename = "";
-            Console.WriteLine("{\n\t\"sitename\" : \"aila2-web\",\n\t\"schema_version\" : 1,\n\t\"file_list\" : [");
-            //foreach (string f in Directory.GetFiles(dir_path)) {
+
+            StringBuilder json_data = new StringBuilder();
+            json_data.AppendLine("{\n\t\"sitename\" : \"aila2-web\",\n\t\"schema_version\" : 1,\n\t\"file_list\" : [");
+
             string [] files = Directory.GetFiles(dir_path, "*.json");
-            int n = 0;
-            foreach (string f in files) {
-                if (f.ToLower().EndsWith("siteconfig.json")) {
-                    n++;
-                }
-            }
+
             for (int i = 0; i < files.Length; i++) {
-                string f = files[i];
-                filename = f.Substring(f.LastIndexOf("\\") + 1);
-                if (filename.EndsWith(".json") && filename != "siteconfig.json") {
-                    if (i == files.Length - n) {
-                        Console.WriteLine("\t\t\"{0}\"", filename);
-                    } else {
-                        Console.WriteLine("\t\t\"{0}\",", filename);
-                    }
+                if (files[i].EndsWith("siteconfig.json")) {
+                    continue;
                 }
+                filename = files[i].Substring(files[i].LastIndexOf("\\") + 1);
+                json_data.AppendFormat("\t\t\"{0}\",\n", filename);
             }
-            Console.WriteLine("\t]\n}");
+
+            json_data.Length = json_data.Length - 3; // Remove the last comma + LF + CR
+            json_data.AppendLine("\n\t]\n}");
+
+            Console.WriteLine(json_data.ToString());
+            return 0;
         }
     }
 }
